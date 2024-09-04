19aa136ef5292260b3dec06ad20af519a03c973a
sahil01012007 <khrbseema@gmail.com>
2024-09-04 14:15:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js code as a newer version
+
+// alert( 3 + 3) // we are using nodejs ,not browser
+
+console.log( 3+
+    3)         // code readability should be high
+
+    console.log("sahil")
+
+
+    let name = "sahil"
+    let age = 17
+    let isloggedin = false
+
+    // number = 2 to power 53
+    // bigint
+    // string = ""
+    // boolean = true/false
+    // null = standalone value
+    // undefined 
+    // symbol = unique
+
+
+    // object
+
+    console.log(typeof null); //object
+    console.log(typeof undefined); // undefined
+    
+    
+    
