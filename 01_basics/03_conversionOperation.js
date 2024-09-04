5b294b0bf84668df1c8b4b868814e6e5b54a9818
sahil01012007 <khrbseema@gmail.com>
2024-09-04 16:23:26 +0000

    ..

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
-// let score = undefined
+ //let score = ""
 
-// console.log(typeof score);
+ //console.log(typeof score);
 // console.log(typeof(score));
 
 // let valueInNumber = Number(score)
@@ -13,16 +13,53 @@
 // null => 0
 // undefined => NaN
 
-// let isLoggedIn = "sahil"
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //let isLoggedIn = ""
+ //let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
 // 1=> true; 0 => false;
 // ""  => fasle
 // "sahil" => true
 
-let somenumber = 1
+// let somenumber = 1
 
-let somenumberinstring = String(somenumber)
-console.log(somenumberinstring);
-console.log(typeof somenumberinstring);
+// let somenumberinstring = String(somenumber)
+// console.log(somenumberinstring);
+// console.log(typeof somenumberinstring);
+
+
+// ******************Operations*************
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(3/2);
+// console.log(3%2);
+
+// let str1 = "hi"
+// let str2 = " sahil"
+// let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log( 1+1+"2");
+// console.log("2" +1+1);
+// console.log( ("2"+"2")+1+1+ +1+1);
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
