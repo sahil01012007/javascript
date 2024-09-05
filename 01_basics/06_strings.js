b248fe107e18c5f0cfb67369f06f7019709d4de1
sahil01012007 <khrbseema@gmail.com>
2024-09-05 13:45:24 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,35 @@
+const name = "sahil"
+const repoCount = 50
+
+console.log(name + repoCount + "50");
+
+console.log(`my name is ${name}kharb`);
+
+const gameName = new String(`sahil-sk-com`) // another method of representing string but in object form
+// output of above will be [String: 'sahil-sk-com']
+
+// console.log(name[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf(`h`));
+
+console.log(gameName.substring(0,4));
+
+const newstring = "   sahil   "
+console.log(newstring.trim()); // trim witespaces
+ 
+console.log(gameName.replace(`s`,`a`));
+console.log(gameName.includes(`sahil`));
+console.log(gameName.split(`-`));
+
+
+
+
+
+
+
+
+
+
