9c2786a9d39a601f1497fdc120e76130738760bd
sahil01012007 <khrbseema@gmail.com>
2024-09-11 16:21:02 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let num = 1
+while (num<=5) {    
+    console.log(num);
+    num = num+1
+}
+
+let myArray = ["a", "b","c"]
+let index = 0
+while (index<myArray.length) {
+    console.log(myArray[index]);
+    index = index +1
+}
+
+let i = 0
+// agr let = 11 hai , then output will be """value is 11"""
+do {console.log(`value is ${i}`);
+    i = i+2
+    
+} while (i<=10);
