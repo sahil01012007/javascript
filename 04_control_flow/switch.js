2d4585bcc6c08385d537674f0a996b629e1426d8
sahil01012007 <khrbseema@gmail.com>
2024-09-11 14:07:05 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("mar");
+        
+        break;
+
+    default: console.log("default");
+
+    
+        break;
+}
