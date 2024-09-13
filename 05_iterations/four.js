791b70cbda3ae78e43fb9cc321605be97e659ec7
sahil01012007 <khrbseema@gmail.com>
2024-09-13 05:01:54 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// for in loop
+
+//const keyyObject = {
+   // name: "sahil",
+   // age: "17"
+//}
+//for (const key in MyObject) {
+    //console.log(`${key} - ${MyObject[key]}`);
+    
+    
+//}
+
+const ARR = ["a","b","c","d"]
+for (const key in ARR) {
+   console.log(ARR[key]);
+   
+}
+
+// below code will not work
+
+// const MyMap = new Map()     // maps are not iterable
+// MyMap.set('IND','HINDI')
+// MyMap.set('UK','ENGLISH')
+// MyMap.set('GERMANY','GERMAN')
+
+// // for (const key in MyMap) {
+// //    console.log(key);
+   
+// // }
