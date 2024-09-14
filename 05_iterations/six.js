2a37c2eb0e9dc4aa4120ec461615656adebd4111
sahil01012007 <khrbseema@gmail.com>
2024-09-14 07:39:30 +0000

    for each

---
+++ 05_iterations/six.js
@@ -0,0 +1,41 @@
+//const nums = [1,2,3,4,5,6,7,8,9]
+// const values =  nums.forEach( (val) => {             // forEach doesnot return value outside the scope, thats why using filter
+//     //console.log(val);
+//     return val
+// } )
+
+//console.log(values);
+
+
+// const newvalues = nums.filter( (num) => num>4 )   // will run
+// console.log(newvalues);
+
+// const newvalues = nums.filter( (num) => {         // will not run bec scope open hogya ha, isilye agr uske bahar value chaiye toh return krna padega
+//     num>4
+// } )
+// console.log(newvalues);
+
+// const newvalues = nums.filter( (num) => {            // will run
+//     return num>4
+// } )
+//console.log(newvalues);
+
+
+
+// if we still want to use forEach 
+
+
+// const nums = [1,2,3,4,5,6,7,8,9]
+// const newnums = []
+
+// nums.forEach( (val) => {
+//     if (val>4) {
+//         newnums.push(val)
+        
+//     }
+// } )
+// console.log(newnums);
+
+
+
+
