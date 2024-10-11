69759c60857a8acca830c4659d3508e146d7d236
sahil01012007 <khrbseema@gmail.com>
2024-10-11 14:02:48 +0000

    protoype.js

---
+++ 10_classes_and_OOP/prototype.js
@@ -0,0 +1,72 @@
+// let myname = "sahil    "
+// let email = "sk@google.com     "
+
+// console.log(myname.trueLength());
+// above is task
+
+
+let myHeoes = ["thor","spiderman"]
+
+let heroes = {
+    thor: "hammer",
+    spiderman: "sling",
+    getSpidyPower: function(){
+        console.log(`spidy power is ${this.spiderman}` );
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log("hitesh is present in all");
+}
+myHeoes.hitesh()
+heroes.hitesh()
+
+Array.prototype.heyHitesh = function(){
+    console.log("hitesh in array");
+    
+}
+myHeoes.heyHitesh()
+
+
+
+// inheritance
+
+const User = {
+    name: "sahil"
+}
+const Teacher = {
+    makevideo: true
+} 
+const TeachingSupport = {
+    isAvailable : true
+}
+const TASupport = {
+    FullTime: true,
+    makeAssisment: "JS Assisment",
+    __proto__: TeachingSupport,
+}
+
+Teacher.__proto__ = User
+
+
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+//                      kon          , kiski prop.  access krega
+
+
+
+// above task solution
+
+let myname = "sahil    "
+String.prototype.trueLength = function(){
+    // console.log(`true length is ${myname.trim()}`);
+    console.log(`true length is ${this.trim()}`);
+    
+    
+}
+myname.trueLength()
+
+let email = "sk@google.com    "
+email.trueLength()
